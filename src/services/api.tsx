2860e9cb8dfc7ca71ab14f95e9a1fb29264f8eb9
Odair <odairodriguez@yahoo.com.br>
2023-11-14 13:40:10 -0300

    feat: change back end

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,7 +1,8 @@
-import axios from "axios"
+import axios from "axios";
 
 export const api = axios.create({
-    // baseURL: 'https://db-desa-app.onrender.com/api/',
-    baseURL: 'http://127.0.0.1:8000/api/',
-    timeout: 8000,
-})
+  // baseURL: 'https://db-desa-app.onrender.com/api/',
+  // baseURL: 'http://127.0.0.1:8000/api/',
+  baseURL: "http://localhost:3000",
+  timeout: 8000,
+});
