01a69bcbcd5929380e1b152781c1556aa20b9045
Odair <odairodriguez@yahoo.com.br>
2023-11-14 13:53:33 -0300

    feat: closed my schedules

--- src/components/Sandwich/index.tsx
+++ src/components/Sandwich/index.tsx
@@ -1,26 +1,28 @@
-'use client';
-import styles from './styles.module.scss';
-import { useState } from 'react';
-import { GlobalContext } from '@/providers/GlobalContext';
-import { useContext } from 'react';
+"use client";
+import styles from "./styles.module.scss";
+import { useState } from "react";
+import { GlobalContext } from "@/providers/GlobalContext";
+import { useContext } from "react";
 
-export default function Sandwich(){
-    const { modalMobile, setModalMobile } = useContext(GlobalContext);
-    const [change, setChange] = useState(0);
-    
-    function handleClickLeft() {
-        if(modalMobile){
-            setModalMobile(false);
-        }else{
-            setModalMobile(true);
-        }
-    };
+export default function Sandwich() {
+  const { modalMobile, setModalMobile, setModalSchedules } =
+    useContext(GlobalContext);
+  const [change, setChange] = useState(0);
 
-    return(
-        <div className={styles.btnMenu} onClick={()=>handleClickLeft()}>
-            <div className={!modalMobile ? styles.line : styles.line1}></div>
-            <div className={!modalMobile ? styles.line : styles.lineNone}></div>
-            <div className={!modalMobile ? styles.line : styles.line2}></div>
-        </div>
-    )
-};
+  function handleClickLeft() {
+    if (modalMobile) {
+      setModalMobile(false);
+      setModalSchedules(false);
+    } else {
+      setModalMobile(true);
+    }
+  }
+
+  return (
+    <div className={styles.btnMenu} onClick={() => handleClickLeft()}>
+      <div className={!modalMobile ? styles.line : styles.line1}></div>
+      <div className={!modalMobile ? styles.line : styles.lineNone}></div>
+      <div className={!modalMobile ? styles.line : styles.line2}></div>
+    </div>
+  );
+}
