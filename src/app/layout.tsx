b55f011fbfdf7004562f08d5545275e3d0f8bf48
Odair <odairodriguez@yahoo.com.br>
2023-11-14 13:42:51 -0300

    feat: schedule provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,31 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '@/sass/main.scss';
-import { GlobalProvider } from '@/providers/GlobalContext';
-import { UserProvider } from '@/providers/UserContext';
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "@/sass/main.scss";
+import { GlobalProvider } from "@/providers/GlobalContext";
+import { UserProvider } from "@/providers/UserContext";
+import { ScheduleProvider } from "@/providers/ScheduleContext";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'de Sá Incorporações',
-  description: 'Imóveis de Qualidade',
-}
+  title: "de Sá Incorporações",
+  description: "Imóveis de Qualidade",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
         <GlobalProvider>
           <UserProvider>
-            {children}
+            <ScheduleProvider>{children}</ScheduleProvider>
           </UserProvider>
         </GlobalProvider>
       </body>
     </html>
-  )
+  );
 }
