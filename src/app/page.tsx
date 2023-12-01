684403cddcaca3da3a96b62e8d758ad78210075f
Odair <odairodriguez@yahoo.com.br>
2023-12-01 15:14:59 -0300

    new update layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import Description from "@/components/Description";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import Slogan from "@/components/Slogan";
-import Filter from "@/components/filter";
 
 export default function Home() {
   return (
@@ -13,10 +12,8 @@ export default function Home() {
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
       />
       <Background>
-        <Filter>
-          <Header />
-          <Slogan />
-        </Filter>
+        <Header />
+        <Slogan />
       </Background>
       <Description />
       <Footer />
--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -1,10 +1,35 @@
-import styles from './styles.module.scss';
+import styles from "./styles.module.scss";
+import Image from "next/image";
+import LadoDireito from "../../img/frente.png";
+import LadoEsquerdo from "../../img/10.png";
+import Centro from "../../img/9.png";
 
-
-export default function Background({children}: {children: React.ReactNode}){
-    return(
-        <div className={styles.container} id='home'>
-            {children}
-        </div>
-    )
+export default function Background({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className={styles.container} id="home">
+      <Image
+        priority={true}
+        src={LadoDireito}
+        alt="Prédio"
+        className={styles.bgFade}
+      />
+      <Image
+        priority={true}
+        src={LadoEsquerdo}
+        alt="Prédio"
+        className={styles.bgFade}
+      />
+      <Image
+        priority={true}
+        src={Centro}
+        alt="Prédio"
+        className={styles.bgFade}
+      />
+      <div className={styles.filter}>{children}</div>
+    </div>
+  );
 }
